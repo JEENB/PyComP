de94172c4acf74c4f2f2548efb59b31fee928577
Jeenb <bjenish12@gmail.com>
2023-03-15 22:44:09 +0530

    setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="PyComP - Python Compression Library",
+    version="1.0",
+    author="Jenish Raj Bajracharya",
+    author_email="bjenish12@gmail.com",
+    packages=["PyComP"],
+    description="A library of common compressors",
+    license="MIT",
+    install_requires=[
+        "pytest",
+        "numpy",
+        "bitarray",
+        "typing",    
+    ],
+)
